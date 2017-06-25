fb93811ded6982cf7915cd88d1ed02deb52fffd8
Lucas Ondel <lucas.ondel@gmail.com>
2017-06-25 17:24:20 -0400

    Remove docs.

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to AMDTK\u2019s documentation!"],titleterms:{amdtk:0,document:0,indic:0,tabl:0,welcom:0}})
